ac7c3702ad08fbe641f4bfce17a51fd51a694e75
shathiso <shathisontibi@yahoo.com>
2019-04-04 18:19:02 +0200

    final changes 3.0

--- app/Controllers/CustomQuery.php
+++ app/Controllers/CustomQuery.php
@@ -10,13 +10,18 @@ class CustomQuery extends Controller
   public $post_type;
   public $orderby;
   public $posts_per_page;
+  public $title_class_name;
+  public $image_class_name;
+  public $content_class_name;
 
-    
-  public function custom_wp_query($post_type, $orderby, $posts_per_page, $title_class_name, $image_class_name, $content_class_name ) {
+ 
+
+
+  public function custom_wp_query($post_type, $order, $posts_per_page, $title_class_name, $image_class_name, $content_class_name ) {
       
         $args = array(
             'post_type' => $post_type,
-            'orderby'   => $orderby,
+            'order'   => $order,
             'posts_per_page' => $posts_per_page,
         );
 
@@ -24,8 +29,10 @@ class CustomQuery extends Controller
       
       if( $the_query->have_posts()):  
         while( $the_query->have_posts() ): $the_query->the_post();
-           return '<h1 class="' . $title_class_name . '"/> ' . get_the_title(). '</h1>' . ' <div class="' . $image_class_name . '"/> ' . the_post_thumbnail() . '</div>' . '<div class="' . $content_class_name . '/">' . get_the_content() . '</div>';
+           echo '<h1 class="' . $title_class_name . '"/> ' . get_the_title(). '</h1>' . ' <div class="' . $image_class_name . '"/> ' . the_post_thumbnail() . '</div>' . '<div class="' . $content_class_name . '/">' . get_the_content() . '</div>';
         endwhile;
       endif;
+
+      wp_reset_postdata();
   }
 }
